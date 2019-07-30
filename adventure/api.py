1a89ef5e4af461367b0804c650b0a414906511a3
curtis <chubbard022@gmail.com>
2019-07-30 10:46:16 -0700

    implimented say function

--- adventure/api.py
+++ adventure/api.py
@@ -64,4 +64,14 @@ def move(request):
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    message = data['message']
+    room = player.room()
+    players_in_room = room.playerUUIDs(player_id)
+    for p_uuid in players_in_room:
+        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} says {message}'ß})
     return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+

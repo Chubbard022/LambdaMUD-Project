e0e407cb465fe03079ba453d905b6b1915672002
curtis <chubbard022@gmail.com>
2019-07-30 16:10:39 -0700

    creating a 10 by 5 map

--- util/create_world.py
+++ util/create_world.py
@@ -1,47 +1,450 @@
 from django.contrib.auth.models import User
 from adventure.models import Player, Room
 
-
 Room.objects.all().delete()
 
-r_outside = Room(title="Outside Cave Entrance",
-               description="North of you, the cave mount beckons")
+#room description 01
+room_01 = Room(title="room_01",
+              description=" ")
+room_02 = Room(title="room_02",
+              description=" ")
+room_03 = Room(title="room_03",
+              description=" ")
+room_04 = Room(title="room_04",
+              description=" ")
+room_05 = Room(title="room_05",
+              description=" ")                
+room_06 = Room(title="room_06",
+              description=" ")
+room_07 = Room(title="room_07",
+              description=" ")
+room_08 = Room(title="room_08",
+              description=" ")
+room_09 = Room(title="room_09",
+              description=" ")
+room_10 = Room(title="room_10",
+              description=" ") 
+#room description 02
+room_11 = Room(title="room_11",
+              description=" ")
+room_12 = Room(title="room_12",
+              description=" ")
+room_13 = Room(title="room_13",
+              description=" ")
+room_14 = Room(title="room_14",
+              description=" ")
+room_15 = Room(title="room_15",
+              description=" ")                
+room_16 = Room(title="room_16",
+              description=" ")
+room_17 = Room(title="room_17",
+              description=" ")
+room_18 = Room(title="room_18",
+              description=" ")
+room_19 = Room(title="room_19",
+              description=" ")
+room_20 = Room(title="room_20",
+              description=" ")
+#room description 03
+room_21 = Room(title="room_21",
+              description=" ")
+room_22 = Room(title="room_22",
+              description=" ")
+room_23 = Room(title="room_23",
+              description=" ")
+room_24 = Room(title="room_24",
+              description=" ")
+room_25 = Room(title="room_25",
+              description=" ")                
+room_26 = Room(title="room_26",
+              description=" ")
+room_27 = Room(title="room_27",
+              description=" ")
+room_28 = Room(title="room_28",
+              description=" ")
+room_29 = Room(title="room_29",
+              description=" ")
+room_20 = Room(title="room_20",
+              description=" ")            
+#room description 04
+room_31 = Room(title="room_31",
+              description=" ")
+room_32 = Room(title="room_32",
+              description=" ")
+room_33 = Room(title="room_33",
+              description=" ")
+room_34 = Room(title="room_34",
+              description=" ")
+room_35 = Room(title="room_35",
+              description=" ")                
+room_36 = Room(title="room_36",
+              description=" ")
+room_37 = Room(title="room_37",
+              description=" ")
+room_38 = Room(title="room_38",
+              description=" ")
+room_39 = Room(title="room_39",
+              description=" ")
+room_40 = Room(title="room_40",
+              description=" ")               
+#room description 05
+room_41 = Room(title="room_41",
+              description=" ")
+room_42 = Room(title="room_42",
+              description=" ")
+room_43 = Room(title="room_43",
+              description=" ")
+room_44 = Room(title="room_44",
+              description=" ")
+room_45 = Room(title="room_45",
+              description=" ")                
+room_46 = Room(title="room_46",
+              description=" ")
+room_47 = Room(title="room_47",
+              description=" ")
+room_48 = Room(title="room_48",
+              description=" ")
+room_49 = Room(title="room_49",
+              description=" ")
+room_50 = Room(title="room_50",
+              description=" ")                                                         
+#--------------------------------------------------------------------------------------------------------
 
-r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
-passages run north and east.""")
+room_01.save()
+room_02.save()
+room_03.save()
+room_04.save()
+room_05.save()
+room_06.save()
+room_07.save()
+room_08.save()
+room_09.save()
+room_10.save()
 
-r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""")
+room_11.save()
+room_12.save()
+room_13.save()
+room_14.save()
+room_15.save()
+room_16.save()
+room_17.save()
+room_18.save()
+room_19.save()
+room_20.save()
 
-r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
-to north. The smell of gold permeates the air.""")
+room_21.save()
+room_22.save()
+room_23.save()
+room_24.save()
+room_25.save()
+room_26.save()
+room_27.save()
+room_28.save()
+room_29.save()
+room_30.save()
 
-r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""")
+room_31.save()
+room_32.save()
+room_33.save()
+room_34.save()
+room_35.save()
+room_36.save()
+room_37.save()
+room_38.save()
+room_39.save()
+room_40.save()
 
-r_outside.save()
-r_foyer.save()
-r_overlook.save()
-r_narrow.save()
-r_treasure.save()
+room_41.save()
+room_42.save()
+room_43.save()
+room_44.save()
+room_45.save()
+room_46.save()
+room_47.save()
+room_48.save()
+room_49.save()
+room_50.save()
 
-# Link rooms together
-r_outside.connectRooms(r_foyer, "n")
-r_foyer.connectRooms(r_outside, "s")
+# Row 01 
+room_01 connectRoom(room_11,'s')
+room_11 connectRoom(room_01,'n')
 
-r_foyer.connectRooms(r_overlook, "n")
-r_overlook.connectRooms(r_foyer, "s")
+room_11 connectRoom(room_21,'s')
+room_21 connectRoom(room_11,'n')
 
-r_foyer.connectRooms(r_narrow, "e")
-r_narrow.connectRooms(r_foyer, "w")
+room_21 connectRoom(room_31,'s')
+room_31 connectRoom(room_21,'n')
 
-r_narrow.connectRooms(r_treasure, "n")
-r_treasure.connectRooms(r_narrow, "s")
+room_31 connectRoom(room_41,'s')
+room_41 connectRoom(room_31,'n')
+
+# Row 02
+room_02 connectRoom(room_12,'s')
+room_12 connectRoom(room_02,'n')
+
+room_12 connectRoom(room_22,'s')
+room_22 connectRoom(room_12,'n')
+
+room_22 connectRoom(room_32,'s')
+room_32 connectRoom(room_22,'n')
+
+room_32 connectRoom(room_42,'s')
+room_42 connectRoom(room_32,'n')
+
+# Row 03 
+room_03 connectRoom(room_13,'s')
+room_13 connectRoom(room_03,'n')
+
+room_13 connectRoom(room_23,'s')
+room_23 connectRoom(room_13,'n')
+
+room_23 connectRoom(room_33,'s')
+room_33 connectRoom(room_23,'n')
+
+room_33 connectRoom(room_43,'s')
+room_43 connectRoom(room_33,'n')
+
+# Row 04 
+room_04 connectRoom(room_14,'s')
+room_14 connectRoom(room_04,'n')
+
+room_14 connectRoom(room_24,'s')
+room_24 connectRoom(room_14,'n')
+
+room_24 connectRoom(room_34,'s')
+room_34 connectRoom(room_24,'n')
+
+room_34 connectRoom(room_44,'s')
+room_44 connectRoom(room_34,'n')
+
+# Row 05 
+room_05 connectRoom(room_15,'s')
+room_15 connectRoom(room_05,'n')
+
+room_15 connectRoom(room_25,'s')
+room_25 connectRoom(room_15,'n')
+
+room_25 connectRoom(room_35,'s')
+room_35 connectRoom(room_25,'n')
+
+room_35 connectRoom(room_45,'s')
+room_45 connectRoom(room_35,'n')
+
+# Row 06 
+room_06 connectRoom(room_16,'s')
+room_16 connectRoom(room_06,'n')
+
+room_16 connectRoom(room_26,'s')
+room_26 connectRoom(room_16,'n')
+
+room_26 connectRoom(room_36,'s')
+room_36 connectRoom(room_26,'n')
+
+room_36 connectRoom(room_46,'s')
+room_46 connectRoom(room_36,'n')
+
+# Row 07 
+room_07 connectRoom(room_17,'s')
+room_17 connectRoom(room_07,'n')
+
+room_17 connectRoom(room_27,'s')
+room_27 connectRoom(room_17,'n')
+
+room_27 connectRoom(room_37,'s')
+room_37 connectRoom(room_27,'n')
+
+room_37 connectRoom(room_47,'s')
+room_47 connectRoom(room_37,'n')
+
+# Row 08
+room_08 connectRoom(room_18,'s')
+room_18 connectRoom(room_08,'n')
+
+room_18 connectRoom(room_28,'s')
+room_28 connectRoom(room_18,'n')
+
+room_28 connectRoom(room_38,'s')
+room_38 connectRoom(room_28,'n')
+
+room_38 connectRoom(room_48,'s')
+room_48 connectRoom(room_38,'n')
+
+# Row 09
+room_09 connectRoom(room_19,'s')
+room_19 connectRoom(room_09,'n')
+
+room_19 connectRoom(room_29,'s')
+room_29 connectRoom(room_19,'n')
+
+room_29 connectRoom(room_39,'s')
+room_39 connectRoom(room_29,'n')
+
+room_39 connectRoom(room_49,'s')
+room_49 connectRoom(room_39,'n')
+
+# Row 10
+room_10 connectRoom(room_20,'s')
+room_20 connectRoom(room_10,'n')
+
+room_20 connectRoom(room_30,'s')
+room_30 connectRoom(room_20,'n')
+
+room_30 connectRoom(room_40,'s')
+room_40 connectRoom(room_30,'n')
+
+room_40 connectRoom(room_50,'s')
+room_50 connectRoom(room_40,'n')
+
+
+
+# 0-10 e-w connections
+
+room_01 connectRoom(room_02,'e')
+room_02 connectRoom(room_01,'w')
+
+room_02 connectRoom(room_03,'e')
+room_03 connectRoom(room_02,'w')
+
+room_03 connectRoom(room_04,'e')
+room_04 connectRoom(room_03,'w')
+
+room_04 connectRoom(room_05,'e')
+room_05 connectRoom(room_04,'w')
+
+room_05 connectRoom(room_06,'e')
+room_06 connectRoom(room_05,'w')
+
+room_06 connectRoom(room_07,'e')
+room_07 connectRoom(room_06,'w')
+
+room_07 connectRoom(room_08,'e')
+room_08 connectRoom(room_07,'w')
+
+room_08 connectRoom(room_09,'e')
+room_09 connectRoom(room_08,'w')
+
+room_09 connectRoom(room_10,'e')
+room_10 connectRoom(room_09,'w')
+
+# 10-20 e-w connections
+
+room_11 connectRoom(room_12,'e')
+room_12 connectRoom(room_11,'w')
+
+room_12 connectRoom(room_13,'e')
+room_13 connectRoom(room_12,'w')
+
+room_13 connectRoom(room_14,'e')
+room_14 connectRoom(room_13,'w')
+
+room_14 connectRoom(room_15,'e')
+room_15 connectRoom(room_14,'w')
+
+room_15 connectRoom(room_16,'e')
+room_16 connectRoom(room_15,'w')
+
+room_16 connectRoom(room_17,'e')
+room_07 connectRoom(room_16,'w')
+
+room_17 connectRoom(room_18,'e')
+room_18 connectRoom(room_17,'w')
+
+room_18 connectRoom(room_19,'e')
+room_19 connectRoom(room_18,'w')
+
+room_19 connectRoom(room_20,'e')
+room_20 connectRoom(room_19,'w')
+
+# 20-30 e-w connections
+
+room_21 connectRoom(room_22,'e')
+room_22 connectRoom(room_21,'w')
+
+room_22 connectRoom(room_23,'e')
+room_23 connectRoom(room_22,'w')
+
+room_23 connectRoom(room_24,'e')
+room_24 connectRoom(room_23,'w')
+
+room_24 connectRoom(room_25,'e')
+room_25 connectRoom(room_24,'w')
+
+room_25 connectRoom(room_26,'e')
+room_26 connectRoom(room_25,'w')
+
+room_26 connectRoom(room_27,'e')
+room_27 connectRoom(room_26,'w')
+
+room_27 connectRoom(room_28,'e')
+room_28 connectRoom(room_27,'w')
+
+room_28 connectRoom(room_29,'e')
+room_29 connectRoom(room_28,'w')
+
+room_29 connectRoom(room_30,'e')
+room_30 connectRoom(room_29,'w')
+
+# 30-40 e-w connections
+
+room_31 connectRoom(room_32,'e')
+room_32 connectRoom(room_31,'w')
+
+room_32 connectRoom(room_33,'e')
+room_33 connectRoom(room_32,'w')
+
+room_33 connectRoom(room_34,'e')
+room_34 connectRoom(room_33,'w')
+
+room_34 connectRoom(room_35,'e')
+room_35 connectRoom(room_34,'w')
+
+room_35 connectRoom(room_36,'e')
+room_36 connectRoom(room_35,'w')
+
+room_36 connectRoom(room_37,'e')
+room_37 connectRoom(room_36,'w')
+
+room_37 connectRoom(room_38,'e')
+room_38 connectRoom(room_37,'w')
+
+room_38 connectRoom(room_39,'e')
+room_39 connectRoom(room_38,'w')
+
+room_39 connectRoom(room_40,'e')
+room_40 connectRoom(room_39,'w')
+
+# 40-50 e-w connections
+
+room_41 connectRoom(room_42,'e')
+room_42 connectRoom(room_41,'w')
+
+room_42 connectRoom(room_43,'e')
+room_43 connectRoom(room_42,'w')
+
+room_43 connectRoom(room_44,'e')
+room_44 connectRoom(room_43,'w')
+
+room_44 connectRoom(room_45,'e')
+room_45 connectRoom(room_44,'w')
+
+room_45 connectRoom(room_46,'e')
+room_46 connectRoom(room_45,'w')
+
+room_46 connectRoom(room_47,'e')
+room_47 connectRoom(room_46,'w')
+
+room_47 connectRoom(room_48,'e')
+room_48 connectRoom(room_47,'w')
+
+room_48 connectRoom(room_49,'e')
+room_49 connectRoom(room_48,'w')
+
+room_49 connectRoom(room_50,'e')
+room_50 connectRoom(room_49,'w')
+
+
 
 players=Player.objects.all()
 for p in players:
-  p.currentRoom=r_outside.id
+  p.currentRoom=room_01.id
   p.save()
-

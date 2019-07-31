02387db36bc187ccb9d6e0234a3fac1f2cc0ed97
curtis <chubbard022@gmail.com>
2019-07-31 08:59:44 -0700

    fixing bug with connecting rooms

--- util/create_world.py
+++ util/create_world.py
@@ -64,7 +64,7 @@ room_28 = Room(title="room_28",
               description="looking around the room, grass and cobblestone fill every inch the eye can see")
 room_29 = Room(title="room_29",
               description="looking around the room, grass and cobblestone fill every inch the eye can see")
-room_20 = Room(title="room_20",
+room_30 = Room(title="room_20",
               description="looking around the room, grass and cobblestone fill every inch the eye can see")            
 #room description 04
 room_31 = Room(title="room_31",
@@ -166,281 +166,281 @@ room_49.save()
 room_50.save()
 
 # Row 01 
-room_01 connectRoom(room_11,'s')
-room_11 connectRoom(room_01,'n')
+room_01.connectRooms(room_11,'s')
+room_11.connectRooms(room_01,'n')
 
-room_11 connectRoom(room_21,'s')
-room_21 connectRoom(room_11,'n')
+room_11.connectRooms(room_21,'s')
+room_21.connectRooms(room_11,'n')
 
-room_21 connectRoom(room_31,'s')
-room_31 connectRoom(room_21,'n')
+room_21.connectRooms(room_31,'s')
+room_31.connectRooms(room_21,'n')
 
-room_31 connectRoom(room_41,'s')
-room_41 connectRoom(room_31,'n')
+room_31.connectRooms(room_41,'s')
+room_41.connectRooms(room_31,'n')
 
 # Row 02
-room_02 connectRoom(room_12,'s')
-room_12 connectRoom(room_02,'n')
+room_02.connectRooms(room_12,'s')
+room_12.connectRooms(room_02,'n')
 
-room_12 connectRoom(room_22,'s')
-room_22 connectRoom(room_12,'n')
+room_12.connectRooms(room_22,'s')
+room_22.connectRooms(room_12,'n')
 
-room_22 connectRoom(room_32,'s')
-room_32 connectRoom(room_22,'n')
+room_22.connectRooms(room_32,'s')
+room_32.connectRooms(room_22,'n')
 
-room_32 connectRoom(room_42,'s')
-room_42 connectRoom(room_32,'n')
+room_32.connectRooms(room_42,'s')
+room_42.connectRooms(room_32,'n')
 
 # Row 03 
-room_03 connectRoom(room_13,'s')
-room_13 connectRoom(room_03,'n')
+room_03.connectRooms(room_13,'s')
+room_13.connectRooms(room_03,'n')
 
-room_13 connectRoom(room_23,'s')
-room_23 connectRoom(room_13,'n')
+room_13.connectRooms(room_23,'s')
+room_23.connectRooms(room_13,'n')
 
-room_23 connectRoom(room_33,'s')
-room_33 connectRoom(room_23,'n')
+room_23.connectRooms(room_33,'s')
+room_33.connectRooms(room_23,'n')
 
-room_33 connectRoom(room_43,'s')
-room_43 connectRoom(room_33,'n')
+room_33.connectRooms(room_43,'s')
+room_43.connectRooms(room_33,'n')
 
 # Row 04 
-room_04 connectRoom(room_14,'s')
-room_14 connectRoom(room_04,'n')
+room_04.connectRooms(room_14,'s')
+room_14.connectRooms(room_04,'n')
 
-room_14 connectRoom(room_24,'s')
-room_24 connectRoom(room_14,'n')
+room_14.connectRooms(room_24,'s')
+room_24.connectRooms(room_14,'n')
 
-room_24 connectRoom(room_34,'s')
-room_34 connectRoom(room_24,'n')
+room_24.connectRooms(room_34,'s')
+room_34.connectRooms(room_24,'n')
 
-room_34 connectRoom(room_44,'s')
-room_44 connectRoom(room_34,'n')
+room_34.connectRooms(room_44,'s')
+room_44.connectRooms(room_34,'n')
 
 # Row 05 
-room_05 connectRoom(room_15,'s')
-room_15 connectRoom(room_05,'n')
+room_05.connectRooms(room_15,'s')
+room_15.connectRooms(room_05,'n')
 
-room_15 connectRoom(room_25,'s')
-room_25 connectRoom(room_15,'n')
+room_15.connectRooms(room_25,'s')
+room_25.connectRooms(room_15,'n')
 
-room_25 connectRoom(room_35,'s')
-room_35 connectRoom(room_25,'n')
+room_25.connectRooms(room_35,'s')
+room_35.connectRooms(room_25,'n')
 
-room_35 connectRoom(room_45,'s')
-room_45 connectRoom(room_35,'n')
+room_35.connectRooms(room_45,'s')
+room_45.connectRooms(room_35,'n')
 
 # Row 06 
-room_06 connectRoom(room_16,'s')
-room_16 connectRoom(room_06,'n')
+room_06.connectRooms(room_16,'s')
+room_16.connectRooms(room_06,'n')
 
-room_16 connectRoom(room_26,'s')
-room_26 connectRoom(room_16,'n')
+room_16.connectRooms(room_26,'s')
+room_26.connectRooms(room_16,'n')
 
-room_26 connectRoom(room_36,'s')
-room_36 connectRoom(room_26,'n')
+room_26.connectRooms(room_36,'s')
+room_36.connectRooms(room_26,'n')
 
-room_36 connectRoom(room_46,'s')
-room_46 connectRoom(room_36,'n')
+room_36.connectRooms(room_46,'s')
+room_46.connectRooms(room_36,'n')
 
 # Row 07 
-room_07 connectRoom(room_17,'s')
-room_17 connectRoom(room_07,'n')
+room_07.connectRooms(room_17,'s')
+room_17.connectRooms(room_07,'n')
 
-room_17 connectRoom(room_27,'s')
-room_27 connectRoom(room_17,'n')
+room_17.connectRooms(room_27,'s')
+room_27.connectRooms(room_17,'n')
 
-room_27 connectRoom(room_37,'s')
-room_37 connectRoom(room_27,'n')
+room_27.connectRooms(room_37,'s')
+room_37.connectRooms(room_27,'n')
 
-room_37 connectRoom(room_47,'s')
-room_47 connectRoom(room_37,'n')
+room_37.connectRooms(room_47,'s')
+room_47.connectRooms(room_37,'n')
 
 # Row 08
-room_08 connectRoom(room_18,'s')
-room_18 connectRoom(room_08,'n')
+room_08.connectRooms(room_18,'s')
+room_18.connectRooms(room_08,'n')
 
-room_18 connectRoom(room_28,'s')
-room_28 connectRoom(room_18,'n')
+room_18.connectRooms(room_28,'s')
+room_28.connectRooms(room_18,'n')
 
-room_28 connectRoom(room_38,'s')
-room_38 connectRoom(room_28,'n')
+room_28.connectRooms(room_38,'s')
+room_38.connectRooms(room_28,'n')
 
-room_38 connectRoom(room_48,'s')
-room_48 connectRoom(room_38,'n')
+room_38.connectRooms(room_48,'s')
+room_48.connectRooms(room_38,'n')
 
 # Row 09
-room_09 connectRoom(room_19,'s')
-room_19 connectRoom(room_09,'n')
+room_09.connectRooms(room_19,'s')
+room_19.connectRooms(room_09,'n')
 
-room_19 connectRoom(room_29,'s')
-room_29 connectRoom(room_19,'n')
+room_19.connectRooms(room_29,'s')
+room_29.connectRooms(room_19,'n')
 
-room_29 connectRoom(room_39,'s')
-room_39 connectRoom(room_29,'n')
+room_29.connectRooms(room_39,'s')
+room_39.connectRooms(room_29,'n')
 
-room_39 connectRoom(room_49,'s')
-room_49 connectRoom(room_39,'n')
+room_39.connectRooms(room_49,'s')
+room_49.connectRooms(room_39,'n')
 
 # Row 10
-room_10 connectRoom(room_20,'s')
-room_20 connectRoom(room_10,'n')
+room_10.connectRooms(room_20,'s')
+room_20.connectRooms(room_10,'n')
 
-room_20 connectRoom(room_30,'s')
-room_30 connectRoom(room_20,'n')
+room_20.connectRooms(room_30,'s')
+room_30.connectRooms(room_20,'n')
 
-room_30 connectRoom(room_40,'s')
-room_40 connectRoom(room_30,'n')
+room_30.connectRooms(room_40,'s')
+room_40.connectRooms(room_30,'n')
 
-room_40 connectRoom(room_50,'s')
-room_50 connectRoom(room_40,'n')
+room_40.connectRooms(room_50,'s')
+room_50.connectRooms(room_40,'n')
 
 
 
 # 0-10 e-w connections
 
-room_01 connectRoom(room_02,'e')
-room_02 connectRoom(room_01,'w')
+room_01.connectRooms(room_02,'e')
+room_02.connectRooms(room_01,'w')
 
-room_02 connectRoom(room_03,'e')
-room_03 connectRoom(room_02,'w')
+room_02.connectRooms(room_03,'e')
+room_03.connectRooms(room_02,'w')
 
-room_03 connectRoom(room_04,'e')
-room_04 connectRoom(room_03,'w')
+room_03.connectRooms(room_04,'e')
+room_04.connectRooms(room_03,'w')
 
-room_04 connectRoom(room_05,'e')
-room_05 connectRoom(room_04,'w')
+room_04.connectRooms(room_05,'e')
+room_05.connectRooms(room_04,'w')
 
-room_05 connectRoom(room_06,'e')
-room_06 connectRoom(room_05,'w')
+room_05.connectRooms(room_06,'e')
+room_06.connectRooms(room_05,'w')
 
-room_06 connectRoom(room_07,'e')
-room_07 connectRoom(room_06,'w')
+room_06.connectRooms(room_07,'e')
+room_07.connectRooms(room_06,'w')
 
-room_07 connectRoom(room_08,'e')
-room_08 connectRoom(room_07,'w')
+room_07.connectRooms(room_08,'e')
+room_08.connectRooms(room_07,'w')
 
-room_08 connectRoom(room_09,'e')
-room_09 connectRoom(room_08,'w')
+room_08.connectRooms(room_09,'e')
+room_09.connectRooms(room_08,'w')
 
-room_09 connectRoom(room_10,'e')
-room_10 connectRoom(room_09,'w')
+room_09.connectRooms(room_10,'e')
+room_10.connectRooms(room_09,'w')
 
 # 10-20 e-w connections
 
-room_11 connectRoom(room_12,'e')
-room_12 connectRoom(room_11,'w')
+room_11.connectRooms(room_12,'e')
+room_12.connectRooms(room_11,'w')
 
-room_12 connectRoom(room_13,'e')
-room_13 connectRoom(room_12,'w')
+room_12.connectRooms(room_13,'e')
+room_13.connectRooms(room_12,'w')
 
-room_13 connectRoom(room_14,'e')
-room_14 connectRoom(room_13,'w')
+room_13.connectRooms(room_14,'e')
+room_14.connectRooms(room_13,'w')
 
-room_14 connectRoom(room_15,'e')
-room_15 connectRoom(room_14,'w')
+room_14.connectRooms(room_15,'e')
+room_15.connectRooms(room_14,'w')
 
-room_15 connectRoom(room_16,'e')
-room_16 connectRoom(room_15,'w')
+room_15.connectRooms(room_16,'e')
+room_16.connectRooms(room_15,'w')
 
-room_16 connectRoom(room_17,'e')
-room_07 connectRoom(room_16,'w')
+room_16.connectRooms(room_17,'e')
+room_07.connectRooms(room_16,'w')
 
-room_17 connectRoom(room_18,'e')
-room_18 connectRoom(room_17,'w')
+room_17.connectRooms(room_18,'e')
+room_18.connectRooms(room_17,'w')
 
-room_18 connectRoom(room_19,'e')
-room_19 connectRoom(room_18,'w')
+room_18.connectRooms(room_19,'e')
+room_19.connectRooms(room_18,'w')
 
-room_19 connectRoom(room_20,'e')
-room_20 connectRoom(room_19,'w')
+room_19.connectRooms(room_20,'e')
+room_20.connectRooms(room_19,'w')
 
 # 20-30 e-w connections
 
-room_21 connectRoom(room_22,'e')
-room_22 connectRoom(room_21,'w')
+room_21.connectRooms(room_22,'e')
+room_22.connectRooms(room_21,'w')
 
-room_22 connectRoom(room_23,'e')
-room_23 connectRoom(room_22,'w')
+room_22.connectRooms(room_23,'e')
+room_23.connectRooms(room_22,'w')
 
-room_23 connectRoom(room_24,'e')
-room_24 connectRoom(room_23,'w')
+room_23.connectRooms(room_24,'e')
+room_24.connectRooms(room_23,'w')
 
-room_24 connectRoom(room_25,'e')
-room_25 connectRoom(room_24,'w')
+room_24.connectRooms(room_25,'e')
+room_25.connectRooms(room_24,'w')
 
-room_25 connectRoom(room_26,'e')
-room_26 connectRoom(room_25,'w')
+room_25.connectRooms(room_26,'e')
+room_26.connectRooms(room_25,'w')
 
-room_26 connectRoom(room_27,'e')
-room_27 connectRoom(room_26,'w')
+room_26.connectRooms(room_27,'e')
+room_27.connectRooms(room_26,'w')
 
-room_27 connectRoom(room_28,'e')
-room_28 connectRoom(room_27,'w')
+room_27.connectRooms(room_28,'e')
+room_28.connectRooms(room_27,'w')
 
-room_28 connectRoom(room_29,'e')
-room_29 connectRoom(room_28,'w')
+room_28.connectRooms(room_29,'e')
+room_29.connectRooms(room_28,'w')
 
-room_29 connectRoom(room_30,'e')
-room_30 connectRoom(room_29,'w')
+room_29.connectRooms(room_30,'e')
+room_30.connectRooms(room_29,'w')
 
 # 30-40 e-w connections
 
-room_31 connectRoom(room_32,'e')
-room_32 connectRoom(room_31,'w')
+room_31.connectRooms(room_32,'e')
+room_32.connectRooms(room_31,'w')
 
-room_32 connectRoom(room_33,'e')
-room_33 connectRoom(room_32,'w')
+room_32.connectRooms(room_33,'e')
+room_33.connectRooms(room_32,'w')
 
-room_33 connectRoom(room_34,'e')
-room_34 connectRoom(room_33,'w')
+room_33.connectRooms(room_34,'e')
+room_34.connectRooms(room_33,'w')
 
-room_34 connectRoom(room_35,'e')
-room_35 connectRoom(room_34,'w')
+room_34.connectRooms(room_35,'e')
+room_35.connectRooms(room_34,'w')
 
-room_35 connectRoom(room_36,'e')
-room_36 connectRoom(room_35,'w')
+room_35.connectRooms(room_36,'e')
+room_36.connectRooms(room_35,'w')
 
-room_36 connectRoom(room_37,'e')
-room_37 connectRoom(room_36,'w')
+room_36.connectRooms(room_37,'e')
+room_37.connectRooms(room_36,'w')
 
-room_37 connectRoom(room_38,'e')
-room_38 connectRoom(room_37,'w')
+room_37.connectRooms(room_38,'e')
+room_38.connectRooms(room_37,'w')
 
-room_38 connectRoom(room_39,'e')
-room_39 connectRoom(room_38,'w')
+room_38.connectRooms(room_39,'e')
+room_39.connectRooms(room_38,'w')
 
-room_39 connectRoom(room_40,'e')
-room_40 connectRoom(room_39,'w')
+room_39.connectRooms(room_40,'e')
+room_40.connectRooms(room_39,'w')
 
 # 40-50 e-w connections
 
-room_41 connectRoom(room_42,'e')
-room_42 connectRoom(room_41,'w')
+room_41.connectRooms(room_42,'e')
+room_42.connectRooms(room_41,'w')
 
-room_42 connectRoom(room_43,'e')
-room_43 connectRoom(room_42,'w')
+room_42.connectRooms(room_43,'e')
+room_43.connectRooms(room_42,'w')
 
-room_43 connectRoom(room_44,'e')
-room_44 connectRoom(room_43,'w')
+room_43.connectRooms(room_44,'e')
+room_44.connectRooms(room_43,'w')
 
-room_44 connectRoom(room_45,'e')
-room_45 connectRoom(room_44,'w')
+room_44.connectRooms(room_45,'e')
+room_45.connectRooms(room_44,'w')
 
-room_45 connectRoom(room_46,'e')
-room_46 connectRoom(room_45,'w')
+room_45.connectRooms(room_46,'e')
+room_46.connectRooms(room_45,'w')
 
-room_46 connectRoom(room_47,'e')
-room_47 connectRoom(room_46,'w')
+room_46.connectRooms(room_47,'e')
+room_47.connectRooms(room_46,'w')
 
-room_47 connectRoom(room_48,'e')
-room_48 connectRoom(room_47,'w')
+room_47.connectRooms(room_48,'e')
+room_48.connectRooms(room_47,'w')
 
-room_48 connectRoom(room_49,'e')
-room_49 connectRoom(room_48,'w')
+room_48.connectRooms(room_49,'e')
+room_49.connectRooms(room_48,'w')
 
-room_49 connectRoom(room_50,'e')
-room_50 connectRoom(room_49,'w')
+room_49.connectRooms(room_50,'e')
+room_50.connectRooms(room_49,'w')
 
 
 
